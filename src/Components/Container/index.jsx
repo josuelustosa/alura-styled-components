5d2824e595884ad597349be1f98d8f35de947255
Josué Lustosa <josuelustosa.job@gmail.com>
2022-06-18 01:04:48 -0400

    :bug: removing quotes from color value

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -6,7 +6,7 @@ import Conta from "../Conta";
 import Extrato from "../Extrato";
 
 const Container = styled.div`
-  background-color: "#f1f1f1";
+  background-color: #f1f1f1;
   min-height: 90vh;
   padding: 0px 15vw;
 `;
