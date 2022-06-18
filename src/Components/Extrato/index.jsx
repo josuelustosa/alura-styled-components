781983b392f0d94a753fade11b32185fd140b593
Josué Lustosa <josuelustosa.job@gmail.com>
2022-06-18 00:25:31 -0400

    :construction: creating components Item and Items and applying props in components

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import { Box, Botao } from "../UI";
 import { extratoLista } from "../../info";
+import Items from "../Items";
 
 export const Extrato = () => {
   return (
     <Box>
-        {
-            extratoLista.updates.map(({id, type, from, value, date}) => {
-                return (
-                    <div key={id}>
-                        <div>{type}</div>
-                        <div>{from}</div>
-                    </div>
-                );
-            })
-        }
-        <Botao>Ver Mais</Botao>
-    </Box>    
-  )
+      {extratoLista.updates.map(({ id, type, from, value, date }) => {
+        return (
+          <Items key={id} type={type} from={from} value={value} date={date} />
+        );
+      })}
+      <Botao>Ver Mais</Botao>
+    </Box>
+  );
 };
 
 export default Extrato;
