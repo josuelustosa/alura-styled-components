268841b6cfda56c1d52148200b6e1c3ccea6f8dc
Josué <josue.filho@conecthus.org.br>
2022-06-20 12:06:19 -0400

    :construction: creating themes

--- src/Components/UI/variaveis.js
+++ src/Components/UI/variaveis.js
@@ -1,3 +1,9 @@
-export const corPrimaria = "#41d3be"
-export const fundoClaro = "#f1f1f1"
-export const textoFundoClaro = "#808080"
+export const corPrimaria = "#41d3be";
+
+export const fundoClaro = "#f1f1f1";
+export const conteudoClaro = "#ffffff";
+export const textoFundoClaro = "#808080";
+
+export const fundoEscuro = "#363537";
+export const conteudoEscuro = "#5c5b5e";
+export const textoFundoEscuro = "#fafafa";
