3712c947f6aab09b02b506755ca1a308015d71cd
Josué <josue.filho@conecthus.org.br>
2022-06-20 14:20:41 -0400

    :construction: altering theme icon color

--- src/Components/ImageFilter.js
+++ src/Components/ImageFilter.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Icone } from "../Components/UI"
+import { IconeTema } from "../Components/UI"
 import alimentacao from "../assets/images/alimentacao.svg"
 import utilidades from "../assets/images/utilidades.svg"
 import saude from "../assets/images/saude.svg"
@@ -9,11 +9,11 @@ import outros from "../assets/images/outros.svg"
 
 export default (type) => {
     const Images = {
-        Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
-        Utilidades: <Icone src={utilidades} alt="Utilidades" />,
-        Saude: <Icone src={saude} alt="Saúde" />,
-        Transporte: <Icone src={transporte} alt="Transporte" />,
-        Outros: <Icone src={outros} alt="Outros" />,
+        Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+        Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
+        Saude: <IconeTema src={saude} alt="Saúde" />,
+        Transporte: <IconeTema src={transporte} alt="Transporte" />,
+        Outros: <IconeTema src={outros} alt="Outros" />,
     }
 
     return Images[type] || Images.Outros;
